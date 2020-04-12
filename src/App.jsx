42f87cffd6e57eac862447188633ca0fcd962218
rahulsai1999 <noneuser2183@gmail.com>
2020-04-12 12:12:53 +0530

    API Request example

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import "./App.css";
 import { Store } from "./utils/store";
+import Axios from "axios";
 
 function App() {
   //The useContext hook provides access to the data present
   //as the value of Context Provider.
-  const store = React.useContext(Store);
+  const { state, dispatch } = React.useContext(Store);
+  const API_URL =
+    "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes";
+
+  // loads the episodes data once the componentMounts
+  React.useEffect(() => {
+    state.episodes.length == 0 &&
+      Axios.get(API_URL).then((response) => {
+        const { _embedded } = response.data;
+        dispatch({
+          type: "FETCH_DATA",
+          payload: _embedded.episodes,
+        });
+      });
+  }, []);
+
   return (
     <React.Fragment>
       <div>
         <h1>Episodes</h1>
         <p>Select your favourite episodes</p>
-        {store}
+        {console.log(state)}
       </div>
     </React.Fragment>
   );
--- src/utils/store.js
+++ src/utils/store.js
@@ -3,12 +3,27 @@ import React from "react";
 //creates a context object which childern components subscribe to.
 const Store = React.createContext();
 
-const initialState = {};
-const reducer = () => {};
+const initialState = {
+  episodes: [],
+  favourites: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_DATA":
+      return { ...state, episodes: action.payload };
+    default:
+      return state;
+  }
+};
 
 //encapsultaes the other components, props provides access to the children components.
 const StoreProvider = (props) => {
-  return <Store.Provider value="Store Data">{props.children}</Store.Provider>;
+  // useReducer hook takes arguments of the reducer and the initial state,
+  // and returns an array of state (data inside store) and dispatch
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const value = { state, dispatch };
+  return <Store.Provider value={value}>{props.children}</Store.Provider>;
 };
 
 export { Store, StoreProvider };
