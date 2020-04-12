95dae0ce9e63225b7e87f80970840417520816fe
rahulsai1999 <noneuser2183@gmail.com>
2020-04-12 14:15:27 +0530

    Updates

---
+++ precache-manifest.c4f7e1e41ffe099dc417e9bdea3af6d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ada4a87c6cb53a27c3f3cf1536af7ee",
+    "url": "/no-redux/index.html"
+  },
+  {
+    "revision": "fae68da5d40a8418d88f",
+    "url": "/no-redux/static/css/2.4af70dfe.chunk.css"
+  },
+  {
+    "revision": "b695c1c9922d5da59881",
+    "url": "/no-redux/static/css/main.794476a0.chunk.css"
+  },
+  {
+    "revision": "fae68da5d40a8418d88f",
+    "url": "/no-redux/static/js/2.f8dc8d5e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/no-redux/static/js/2.f8dc8d5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b695c1c9922d5da59881",
+    "url": "/no-redux/static/js/main.2f18b6ef.chunk.js"
+  },
+  {
+    "revision": "1d6288dc2b92dc90c969",
+    "url": "/no-redux/static/js/runtime-main.fed7aaf4.js"
+  }
+]);
